60bb6c0f8622eb7de529306a1e7ba1359cadcf27
shubhagk <shubhagirish@gmail.com>
2026-03-30 14:38:38 +0530

    updated server port

--- server.js
+++ server.js
@@ -88,4 +88,8 @@ app.get("/categories", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
